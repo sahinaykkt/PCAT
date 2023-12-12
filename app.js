a6d5b43074bcef67708227958c25343f32c468e9
sahinaykkt <sahinaykkt@gmail.com>
2023-12-13 01:21:49 +0300

    Static files uploaded.

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-const fn = () => {
-  console.log('Function Working');
-};
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is working on the port ${port}.`);
+});

0aa5ceae10d56f890059eeb986b575be769ac567
sahinaykkt <sahinaykkt@gmail.com>
2024-01-10 14:44:32 +0300

    Latest update.

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const ejs = require('ejs');
 const path = require('path');
+const fs = require('fs')
+const app = express();
+
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+app.get('/announcements', (req, res) => {
+  const jsonData = fs.readFileSync('./data.json', 'utf8');
+
+  res.send(jsonData);
 });
 
+app.post('/photos', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+const port = 3000;
 app.listen(port, () => {
-  console.log(`Server is working on the port ${port}.`);
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
 });
